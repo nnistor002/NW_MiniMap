8a1ed993c5ea3ae88093afc68ef04b123bcfadc4
Natanael Nistor <NatanaelViorelNistor@Gmail.com>
2021-09-12 01:04:54 -0400

    worked on the icon scale based on zoom of map

--- src/components/Map.js
+++ src/components/Map.js
@@ -23,13 +23,15 @@ class Map extends React.Component {
   }
 
   componentDidMount() {
-    window.addEventListener("scroll", () => {
-      console.log("Works??");
-    });
-
     document
       .getElementById("full_Map")
       .addEventListener("mousemove", (e) => this.handleMousePosition(e));
+  }
+
+  componentDidUpdate() {
+    this.handleIconScale(
+      document.getElementById("full_Map").getBoundingClientRect().width
+    );
   }
 
   handleMousePosition(e) {
@@ -54,7 +56,40 @@ class Map extends React.Component {
     });
   }
 
-  handleIconScale() {}
+  handleIconScale(newMapWH) {
+    let iconArray = document.getElementsByClassName("iconDiv");
+
+    if (newMapWH > 750 && newMapWH < 1250) {
+      for (let x = 0; x < iconArray.length; x++) {
+        iconArray[x].style.transform = "scale(4)";
+      }
+    }
+    if (newMapWH > 1250 && newMapWH < 1750) {
+      for (let x = 0; x < iconArray.length; x++) {
+        iconArray[x].style.transform = "scale(3)";
+      }
+    }
+    if (newMapWH > 1750 && newMapWH < 2500) {
+      for (let x = 0; x < iconArray.length; x++) {
+        iconArray[x].style.transform = "scale(2.5)";
+      }
+    }
+    if (newMapWH > 2500 && newMapWH < 3700) {
+      for (let x = 0; x < iconArray.length; x++) {
+        iconArray[x].style.transform = "scale(2)";
+      }
+    }
+    if (newMapWH > 3700 && newMapWH < 4500) {
+      for (let x = 0; x < iconArray.length; x++) {
+        iconArray[x].style.transform = "scale(1.5)";
+      }
+    }
+    if (newMapWH > 4500 && newMapWH < 6100) {
+      for (let x = 0; x < iconArray.length; x++) {
+        iconArray[x].style.transform = "scale(1)";
+      }
+    }
+  }
 
   render() {
     return (
@@ -65,14 +100,18 @@ class Map extends React.Component {
             value={this.state.value}
             onChange={(value) => this.setState({ value })}
             minScale={0.2}
-            maxScale={1.1}
+            maxScale={1}
           >
             <div
               id="overLayDiv"
               width={this.state.imgSize}
               height={this.state.imgSize}
             >
-              <div id="testIcon" title="IronOre"></div>
+              <div className="iconDiv" id="testIcon"></div>
+              <div className="iconDiv" id="testIconOne"></div>
+              <div className="iconDiv" id="testIconTwo"></div>
+              <div className="iconDiv" id="testIconThree"></div>
+              <div className="iconDiv" id="testIconFour"></div>
             </div>
             <img
               id="full_Map"
