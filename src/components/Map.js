7ba6f698b123592e37218ff858994ad0e4c1324b
Natanael Nistor <NatanaelViorelNistor@Gmail.com>
2021-09-21 18:58:53 -0400

    worked more on the roundmenu category btns and sub category options

--- src/components/Map.js
+++ src/components/Map.js
@@ -22,7 +22,6 @@ import farming from "../images/menuIcons/Farming.png";
 import mobs from "../images/menuIcons/Mobs.png";
 
 import { MapInteractionCSS } from "react-map-interaction";
-import { cosh } from "core-js/core/number";
 
 class Map extends React.Component {
   constructor(props) {
@@ -38,7 +37,25 @@ class Map extends React.Component {
           y: 0,
         },
       },
-      zoomState: false,
+      zoomStateMain: false,
+      zoomStateMainCategory: {
+        zoomNotePinBTN: false,
+        zoomMobBTN: false,
+        zoomMiningBTN: false,
+        zoomHarvestingBTN: false,
+        zoomLoggingBTN: false,
+        zoomTrackingBTN: false,
+        zoomFarmingBTN: false,
+        zoomFishingBTN: false,
+      },
+      zoomStateNoteSubCat: false,
+      zoomStateMobSubCat: false,
+      zoomStateMiningSubCat: false,
+      zoomStateHarvestingSubCat: false,
+      zoomStateLoggingSubCat: false,
+      zoomStateTrackingSubCat: false,
+      zoomStateFarmingSubCat: false,
+      zoomStateFishingSubCat: false,
       windowMousePosX: 0,
       windowMousePosY: 0,
     };
@@ -52,6 +69,38 @@ class Map extends React.Component {
       .addEventListener("click", () => this.closeBTN());
 
     document
+      .getElementById("notePinBTN")
+      .addEventListener("click", () => this.notePinBTN());
+
+    document
+      .getElementById("mobBTN")
+      .addEventListener("click", () => this.mobPinBTN());
+
+    document
+      .getElementById("miningBTN")
+      .addEventListener("click", () => this.miningBTN());
+
+    document
+      .getElementById("harvestingBTN")
+      .addEventListener("click", () => this.harvestingBTN());
+
+    document
+      .getElementById("loggingBTN")
+      .addEventListener("click", () => this.loggingBTN());
+
+    document
+      .getElementById("trackingBTN")
+      .addEventListener("click", () => this.trackingBTN());
+
+    document
+      .getElementById("farmingBTN")
+      .addEventListener("click", () => this.farmingBTN());
+
+    document
+      .getElementById("fishingBTN")
+      .addEventListener("click", () => this.fishingBTN());
+
+    document
       .getElementById("full_Map")
       .addEventListener("auxclick", (e) => this.handleMiddleClickMenu(e));
 
@@ -61,7 +110,7 @@ class Map extends React.Component {
 
     setTimeout(function () {
       document.getElementById("WelcomeDiv").hidden = true;
-    }, 7000);
+    }, 2000);
   }
 
   componentDidUpdate() {
@@ -180,19 +229,197 @@ class Map extends React.Component {
       // let mapOffSetY = e.offsetY;
 
       // CREATE BORDER LOGIC IF CLICK IS NEXT TO SIDE OF SCREEN
-      z.style.left = this.state.windowMousePosX - 250 + "px";
-      z.style.top = this.state.windowMousePosY - 250 + "px";
+      z.style.left = this.state.windowMousePosX - 360 + "px";
+      z.style.top = this.state.windowMousePosY - 350 + "px";
 
       this.setState({
-        zoomState: true,
+        zoomStateMain: true,
       });
+      this.toggleMainCategory(true);
     }
   }
 
   closeBTN() {
     this.setState({
-      zoomState: false,
+      zoomStateNoteSubCat: false,
+      zoomStateMobSubCat: false,
+      zoomStateMiningSubCat: false,
+      zoomStateHarvestingSubCat: false,
+      zoomStateLoggingSubCat: false,
+      zoomStateTrackingSubCat: false,
+      zoomStateFarmingSubCat: false,
+      zoomStateFishingSubCat: false,
+      zoomStateMain: false,
     });
+    this.toggleMainCategory(false);
+  }
+
+  toggleMainCategory(bol) {
+    if (bol === true) {
+      this.setState({
+        zoomStateMainCategory: {
+          zoomNotePinBTN: true,
+          zoomMobBTN: true,
+          zoomMiningBTN: true,
+          zoomHarvestingBTN: true,
+          zoomLoggingBTN: true,
+          zoomTrackingBTN: true,
+          zoomFarmingBTN: true,
+          zoomFishingBTN: true,
+        },
+      });
+    }
+
+    if (bol === false) {
+      this.setState({
+        zoomStateMainCategory: {
+          zoomNotePinBTN: false,
+          zoomMobBTN: false,
+          zoomMiningBTN: false,
+          zoomHarvestingBTN: false,
+          zoomLoggingBTN: false,
+          zoomTrackingBTN: false,
+          zoomFarmingBTN: false,
+          zoomFishingBTN: false,
+        },
+      });
+    }
+  }
+
+  notePinBTN() {
+    if (this.state.zoomStateNoteSubCat === false) {
+      this.toggleMainCategory(false);
+      this.setState({
+        zoomStateNoteSubCat: true,
+        zoomStateMainCategory: {
+          zoomNotePinBTN: true,
+        },
+      });
+    } else {
+      this.toggleMainCategory(true);
+      this.setState({
+        zoomStateNoteSubCat: false,
+      });
+    }
+  }
+
+  mobPinBTN() {
+    if (this.state.zoomStateMobSubCat === false) {
+      this.toggleMainCategory(false);
+      this.setState({
+        zoomStateMobSubCat: true,
+        zoomStateMainCategory: {
+          zoomMobBTN: true,
+        },
+      });
+    } else {
+      this.toggleMainCategory(true);
+      this.setState({
+        zoomStateMobSubCat: false,
+      });
+    }
+  }
+
+  miningBTN() {
+    if (this.state.zoomStateMiningSubCat === false) {
+      this.toggleMainCategory(false);
+      this.setState({
+        zoomStateMiningSubCat: true,
+        zoomStateMainCategory: {
+          zoomMiningBTN: true,
+        },
+      });
+    } else {
+      this.toggleMainCategory(true);
+      this.setState({
+        zoomStateMiningSubCat: false,
+      });
+    }
+  }
+
+  harvestingBTN() {
+    if (this.state.zoomStateHarvestingSubCat === false) {
+      this.toggleMainCategory(false);
+      this.setState({
+        zoomStateHarvestingSubCat: true,
+        zoomStateMainCategory: {
+          zoomHarvestingBTN: true,
+        },
+      });
+    } else {
+      this.toggleMainCategory(true);
+      this.setState({
+        zoomStateHarvestingSubCat: false,
+      });
+    }
+  }
+
+  loggingBTN() {
+    if (this.state.zoomStateLoggingSubCat === false) {
+      this.toggleMainCategory(false);
+      this.setState({
+        zoomStateLoggingSubCat: true,
+        zoomStateMainCategory: {
+          zoomLoggingBTN: true,
+        },
+      });
+    } else {
+      this.toggleMainCategory(true);
+      this.setState({
+        zoomStateLoggingSubCat: false,
+      });
+    }
+  }
+
+  trackingBTN() {
+    if (this.state.zoomStateTrackingSubCat === false) {
+      this.toggleMainCategory(false);
+      this.setState({
+        zoomStateTrackingSubCat: true,
+        zoomStateMainCategory: {
+          zoomTrackingBTN: true,
+        },
+      });
+    } else {
+      this.toggleMainCategory(true);
+      this.setState({
+        zoomStateTrackingSubCat: false,
+      });
+    }
+  }
+
+  farmingBTN() {
+    if (this.state.zoomStateFarmingSubCat === false) {
+      this.toggleMainCategory(false);
+      this.setState({
+        zoomStateFarmingSubCat: true,
+        zoomStateMainCategory: {
+          zoomFarmingBTN: true,
+        },
+      });
+    } else {
+      this.toggleMainCategory(true);
+      this.setState({
+        zoomStateFarmingSubCat: false,
+      });
+    }
+  }
+
+  fishingBTN() {
+    if (this.state.zoomStateFishingSubCat === false) {
+      this.toggleMainCategory(false);
+      this.setState({
+        zoomStateFishingSubCat: true,
+        zoomStateMainCategory: {
+          zoomFishingBTN: true,
+        },
+      });
+    } else {
+      this.toggleMainCategory(true);
+      this.setState({
+        zoomStateFishingSubCat: false,
+      });
+    }
   }
 
   render() {
@@ -233,16 +460,16 @@ class Map extends React.Component {
           </div>
           <div id="ringMenuGround">
             <Zoom
-              in={this.state.zoomState}
-              timeout={{ enter: 300, exit: 1000 }}
+              in={this.state.zoomStateMain}
+              timeout={{ enter: 100, exit: 500 }}
             >
               <div id="rightClickMenuContainer">
                 <div id="closeBTN">
                   <img src={exit} alt="Exit" width="35" height="35"></img>
                 </div>
                 <Zoom
-                  in={this.state.zoomState}
-                  timeout={{ enter: 600, exit: 800 }}
+                  in={this.state.zoomStateMainCategory.zoomNotePinBTN}
+                  timeout={{ enter: 200, exit: 500 }}
                 >
                   <div id="notePinBTN">
                     <img
@@ -255,22 +482,28 @@ class Map extends React.Component {
                   </div>
                 </Zoom>
                 <Zoom
-                  in={this.state.zoomState}
-                  timeout={{ enter: 900, exit: 700 }}
+                  in={this.state.zoomStateNoteSubCat}
+                  timeout={{ enter: 200, exit: 500 }}
                 >
-                  <div id="loggingBTN">
+                  <div id="notePinSubDiv"></div>
+                </Zoom>
+                <Zoom
+                  in={this.state.zoomStateMainCategory.zoomMobBTN}
+                  timeout={{ enter: 300, exit: 350 }}
+                >
+                  <div id="mobBTN">
                     <img
-                      src={logging}
-                      alt="Logging"
-                      title="Logging"
+                      src={mobs}
+                      alt="Mobs"
+                      title="Mobs & Elites"
                       width="42"
                       height="42"
                     ></img>
                   </div>
                 </Zoom>
                 <Zoom
-                  in={this.state.zoomState}
-                  timeout={{ enter: 1200, exit: 600 }}
+                  in={this.state.zoomStateMainCategory.zoomMiningBTN}
+                  timeout={{ enter: 400, exit: 300 }}
                 >
                   <div id="miningBTN">
                     <img
@@ -283,8 +516,8 @@ class Map extends React.Component {
                   </div>
                 </Zoom>
                 <Zoom
-                  in={this.state.zoomState}
-                  timeout={{ enter: 1500, exit: 500 }}
+                  in={this.state.zoomStateMainCategory.zoomHarvestingBTN}
+                  timeout={{ enter: 500, exit: 250 }}
                 >
                   <div id="harvestingBTN">
                     <img
@@ -297,24 +530,24 @@ class Map extends React.Component {
                   </div>
                 </Zoom>
                 <Zoom
-                  in={this.state.zoomState}
-                  timeout={{ enter: 1800, exit: 400 }}
+                  in={this.state.zoomStateMainCategory.zoomLoggingBTN}
+                  timeout={{ enter: 600, exit: 200 }}
                 >
-                  <div id="fishingBTN">
+                  <div id="loggingBTN">
                     <img
-                      src={fishing}
-                      alt="Fishing"
-                      title="Fishing"
+                      src={logging}
+                      alt="Logging"
+                      title="Logging"
                       width="42"
                       height="42"
                     ></img>
                   </div>
                 </Zoom>
                 <Zoom
-                  in={this.state.zoomState}
-                  timeout={{ enter: 2100, exit: 300 }}
+                  in={this.state.zoomStateMainCategory.zoomTrackingBTN}
+                  timeout={{ enter: 700, exit: 150 }}
                 >
-                  <div id="animalsBTN">
+                  <div id="trackingBTN">
                     <img
                       src={animal}
                       alt="Tracking"
@@ -325,8 +558,8 @@ class Map extends React.Component {
                   </div>
                 </Zoom>
                 <Zoom
-                  in={this.state.zoomState}
-                  timeout={{ enter: 2400, exit: 200 }}
+                  in={this.state.zoomStateMainCategory.zoomFarmingBTN}
+                  timeout={{ enter: 800, exit: 100 }}
                 >
                   <div id="farmingBTN">
                     <img
@@ -339,14 +572,14 @@ class Map extends React.Component {
                   </div>
                 </Zoom>
                 <Zoom
-                  in={this.state.zoomState}
-                  timeout={{ enter: 2700, exit: 100 }}
+                  in={this.state.zoomStateMainCategory.zoomFishingBTN}
+                  timeout={{ enter: 900, exit: 50 }}
                 >
-                  <div id="mobBTN">
+                  <div id="fishingBTN">
                     <img
-                      src={mobs}
-                      alt="Mobs"
-                      title="Mobs & Elites"
+                      src={fishing}
+                      alt="Fishing"
+                      title="Fishing"
                       width="42"
                       height="42"
                     ></img>
